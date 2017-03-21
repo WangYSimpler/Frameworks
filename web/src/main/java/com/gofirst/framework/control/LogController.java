9a6e0f108020cf41224bde994f72d66de0410787
Simpler <wangyongsd2011@163.com>
2017-03-21 17:34:57 +0800

    web 系统的配置修改

--- web/src/main/java/com/gofirst/framework/control/LogController.java
+++ web/src/main/java/com/gofirst/framework/control/LogController.java
@@ -1,14 +1,23 @@
 package com.gofirst.framework.control;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gofirst.framework.bean.UserForm;
 
 
 /**
@@ -22,27 +31,35 @@ public class LogController {
 
 	private static Logger logger = Logger.getLogger(LogController.class);
 
+	
 	/**
-	 * 跳转登录页
-	 */
-	@RequestMapping(value = "toLogin")
-	public String toLogin(HttpServletRequest request) {
-
-		return "login";
-	}
-
-	/**
-	 * 登录
+	 * 
+	 * @param no
+	 * @param pwd
+	 * @param request
+	 * @param session
+	 * @return
 	 */
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login(String no, String pwd, HttpServletRequest request, HttpSession session) {
+	public  ModelAndView login(String no, String pwd, HttpServletRequest request, HttpSession session) {
 		logger.info("登录成功");
 
-		return "/welcome";
+		return new ModelAndView("index");
 	}
 	
 	
-	
+	@RequestMapping(value="/test",method=RequestMethod.POST)
+	public @ResponseBody Map<String, Object> testPostJson(@RequestBody  UserForm userForm,BindingResult bindingResult) {
+			
+		Map<String, Object> map = new HashMap<String, Object>();
+		if (bindingResult.hasErrors()) {
+			map.put("errorCode", "40001");
+			map.put("errorMsg", bindingResult.getFieldError().getDefaultMessage());
+		}
+		
+		map.put("user", userForm);
+		return map;
+	}
 	
 	@RequestMapping(value="/logout", method = RequestMethod.DELETE)
 	public void logout(HttpServletRequest request, HttpServletResponse response) {
